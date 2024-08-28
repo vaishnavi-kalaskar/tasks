5afabf44d34abb5593b68e401bbbcdaf9e3b92a9
vaishnavi-kalaskar <vaishnavikalskar02@gmail.com>
2024-08-28 18:17:01 +0530

    Add files via upload

---
+++ 18-aug-2024-math-module.js.js
@@ -0,0 +1,74 @@
+console.log(Math.PI);
+// output: 3.141592653589793 (22/7)
+
+console.log(Math.E);
+// output: 2.718281828459045
+
+a = 5.2;
+console.log(a);
+//output:5.2
+
+console.log(Math.floor(a));
+//output:5
+
+console.log(Math.ceil(a));
+//output:6
+
+console.log(Math.trunc(a));
+//output:5
+
+console.log(Math.round(a));
+//output:5
+
+console.log(Math.max(100, 20, 67, 45));
+//output:100
+
+console.log(Math.min(9, 7, 45, 34, 1));
+//output:1
+
+console.log(Math.pow(5, 4));
+//output:625
+
+console.log(Math.random());
+// output: - a random number between 0 and 1
+
+console.log(Math.random() * 10);
+// output: a random number between 0 and 10
+
+console.log(Math.floor(Math.random() * 10));
+// output: a random integer between 0 and 10
+
+console.log(Math.floor(Math.random() * 10) + 1);
+// output: a random integer from 1 to 10
+
+console.log(Math.floor(Math.random() * (30 - 20)) + 20);
+// console.log(Math.floor(Math.random() * (max - min)) + min);
+// output: a random integer from 20 to 30
+
+console.log(Math.floor(Math.random() * (9999 - 1000) + 1000));
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+// output: a random integer from 45 to 95
+
+console.log(Math.sqrt(625));
+// output: 25
+
+console.log(Math.abs(-75));
+// output: 75
+
+console.log(Math.hypot(3, 4)); // 60, 11 ---> 61
+// output: 5
+
+console.log(Math.log(15));
+// output: 2.7080502014981877
+
+console.log(Math.exp(19));
+// output: 6.1125472355015e+8
+
+console.log(Math.sin(Math.PI / 2));
+// output: 1
+
+console.log(Math.cos(45));
+// output: 0.7071067811865476
+
+console.log(Math.tan(60));
+// output: 1.7320508075688772
